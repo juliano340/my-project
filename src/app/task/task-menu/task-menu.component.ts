f7e3aea06b04c71a22c0c7647ca36efec49b6663
Juliano Pereira <jpereira@dataweb.com.br>
2024-12-05 16:51:28 -0300

    FEAT(taskMenu): Adiciona botão para consulta de usuários.

--- src/app/task/task-menu/task-menu.component.ts
+++ src/app/task/task-menu/task-menu.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-task-menu',
   templateUrl: './task-menu.component.html',
-  styleUrl: './task-menu.component.css',
+  styleUrls: ['./task-menu.component.css'],
 })
-export class TaskMenuComponent {}
+export class TaskMenuComponent implements OnInit {
+  isAdmin: boolean = false;
+
+  ngOnInit(): void {
+    const loggedInUser = JSON.parse(
+      localStorage.getItem('loggedInUser') || '{}'
+    );
+    this.isAdmin = loggedInUser?.role === 'admin';
+  }
+}
